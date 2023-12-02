a4e718df43ea1f88435f129c0013256c2c0ac0c8
jose-tl <j.tonnesenlucas@gmail.com>
2023-12-02 16:48:35 +0100

    link test

--- api.js
+++ api.js
@@ -23,6 +23,39 @@ app.get('/cafes',(req,res)=>{
     })
 });
 
+app.get('/users',(req,res)=>{
+    const queryParameter = req.query.type;
+    const q = "SELECT * FROM users";
+    connection.query(q, (error, results)=>{
+        res.send(results);
+    })
+});
+
+app.post('cafes/new',(req,res)=>{
+    const id = req.body.name;
+    const name = req.body.primary;
+
+
+    // Dette er opgave A
+    connection.query('INSERT INTO cafes(cafe_id,cafe_name) VALUES(?,?)',
+        [id,name],
+        (error,result)=>{
+            res.send("Successful POST request");
+        });
+});
+
+app.post('users/new',(req,res)=>{
+    const id = req.body.name;
+    const name = req.body.primary;
+
+
+    // Dette er opgave A
+    connection.query('INSERT INTO cafes(cafe_id,cafe_name) VALUES(?,?)',
+        [id,name],
+        (error,result)=>{
+            res.send("Successful POST request");
+        });
+});
 
 app.get('*',(req,res) =>{
     res.sendStatus(404);
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-//This is how to submit data by a fetch call
-submitButton.addEventListener("click",()=>{
+function sendNewCafe(id,name) {
 
     const jsonObjectToPost = {
         cafe_id:id,
-        cafe_name:'name'
+        cafe_name:name
     }
 
     const fetchConfiguration = {
@@ -17,5 +16,41 @@ submitButton.addEventListener("click",()=>{
     fetch("http://localhost:3000/new", fetchConfiguration)
         .then(res => res.json())
         .then(res => console.log(res));
+}
+
+function sendNewUser(id,name) {
+
+    const jsonObjectToPost = {
+        user_id:id,
+        user_name:name
+    }
+
+    const fetchConfiguration = {
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(jsonObjectToPost)
+    }
+
+    fetch("http://localhost:3000/users/new", fetchConfiguration)
+        .then(res => res.json())
+        .then(res => console.log(res));
+}
+
+const submitButton = document.querySelector(".submit-button");
+
+
+submitButton.addEventListener("click",()=>{
+
+    const cafeID = document.querySelector("#new-cafe-id")
+    const cafeName = document.querySelector("#new-cafe-name")
+    const userID = document.querySelector("#new-user-id")
+    const userName = document.querySelector("#new-user-name")
+
+    sendNewCafe(cafeID,cafeName);
+    sendNewUser(userID,userName)
+
+
 
 });
