cd786cf6c7d56f49482547bb93c0738dfd2465f6
jose-tl <j.tonnesenlucas@gmail.com>
2023-12-05 14:59:24 +0100

    list Creation function

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 const cafeList = document.querySelector("#cafe-list")
 const userList = document.querySelector("#user-list")
+const submitButton = document.querySelector(".submit-button")
 
-
-// gets the search parameters https://www.sitepoint.com/get-url-parameters-with-javascript/
+function createCafeListElement(name,location,cost,noise,group,wifi,food,gluten,veg,pet){
+    const newElement = document.createElement("li");
+    newElement.classList.add("cafe-item")
+    newElement.innerHTML = `<h3>${name}</h3><div class="cafe-wrap"><div class="cafe-main"><p>Location: ${location}</p><p>Cost: ${cost}</p><p>Noise Level: ${noise}</p></div><div class="cafe-tags">${group}</div></div>`
+    cafeList.appendChild(newElement)
+}
 const queryString = window.location.search;
 
 
-fetch("http://localhost:3000/cafes"+queryString)
+// gets the search parameters https://www.sitepoint.com/get-url-parameters-with-javascript/
+function fetchCafeData (){
+    const queryString = window.location.search;
+    fetch("http://localhost:3000/cafes/search"+queryString)
     .then(response => response.json())
     .then(data => {
         data.forEach(e => {
-            const newElement = document.createElement("li");
-            newElement.innerText = e.cafe_name;
-            cafeList.appendChild(newElement);
+            createCafeListElement(e.cafe_name,e.location,e.cost,e.noise,e.group,e.wifi,e.food,e.gluten,e.vegetarian,e.pet)
         });
     });
+}
+
+fetchCafeData()
 
 fetch("http://localhost:3000/users"+queryString)
     .then(response => response.json())
@@ -26,3 +35,7 @@ fetch("http://localhost:3000/users"+queryString)
         });
     });
 
+
+submitButton.addEventListener("click",()=>{
+    fetchCafeData()
+})
