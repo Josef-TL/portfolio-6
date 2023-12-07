7d0c5ca3faf40b4d9e867716fa185d3963dd977d
NikoJulius1 <143094080+NikoJulius1@users.noreply.github.com>
2023-12-07 10:37:44 +0100

    Lavet en user side Lavet login side stylet lidt på user side

---
+++ profile-page.js
@@ -0,0 +1,10 @@
+document.querySelector("#profilePhoto").addEventListener('change', function(event) {
+    document.querySelector("#photoDisplayArea");
+
+var file = event.target.files[0];
+
+if (file) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        displayArea.style.backgroundImage = 'url(' + e.target.result + ')';
+    }}})
