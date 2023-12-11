445efc8b3712285b7c5b63a397eac63d11c917c0
jose-tl <j.tonnesenlucas@gmail.com>
2023-12-11 13:26:04 +0100

    directory cleanup

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './src/new-user.js',
-    output: {
-        filename: 'login.js',
-        path: path.resolve(__dirname, 'Client'),
-    },
-};
