5230d04f647483c719e7ecab3382a99f41d9d0a6
NikoJulius1 <143094080+NikoJulius1@users.noreply.github.com>
2023-12-14 14:37:27 +0100

    Videre stylet new cafe

--- js/api.js
+++ js/api.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const connection = db.createConnection({
     host:"localhost",
     user:"root",
-    password:"Tipsbladet!1997",
+    password:"Buster42Kuller",
     database:"cafes"
 });
 
